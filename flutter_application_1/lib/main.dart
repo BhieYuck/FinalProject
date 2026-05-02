c05b97abec4fc9776fd6307179da6449514bdd57
BhieYuck <dansecobrent@gmail.com>
2026-05-02 17:33:18 +0800

    Update main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'models/item_model.dart';
+
 import 'screens/home_screen.dart';
 import 'screens/inventory_screen.dart';
 import 'screens/stocks_screen.dart';
 import 'screens/reports_screen.dart';
+
+import 'widgets/bottom_nav.dart';
 
 void main() {
   runApp(const StockTrackApp());
@@ -14,9 +18,10 @@ class StockTrackApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const MobileFrame(),
+      home: MobileFrame(),
     );
   }
 }
@@ -25,82 +30,178 @@ class MobileFrame extends StatefulWidget {
   const MobileFrame({super.key});
 
   @override
-  State<MobileFrame> createState() => _MobileFrameState();
+  State<MobileFrame> createState() =>
+      _MobileFrameState();
 }
 
-class _MobileFrameState extends State<MobileFrame> {
+class _MobileFrameState
+    extends State<MobileFrame> {
 
   int currentIndex = 0;
 
-  final screens = const [
-    HomeScreen(),
-    InventoryScreen(),
-    StocksScreen(),
-    ReportsScreen(),
+  final List<ItemModel> items = [
+
+    ItemModel(
+      name: "Rice",
+      quantity: 20,
+      category: "Food",
+      usedCount: 18,
+      expiryDate:
+      DateTime.now().add(
+        const Duration(days: 90),
+      ),
+    ),
+
+    ItemModel(
+      name: "Soap",
+      quantity: 2,
+      category: "Supplies",
+      usedCount: 30,
+      expiryDate:
+      DateTime.now().add(
+        const Duration(days: 14),
+      ),
+    ),
+
+    ItemModel(
+      name: "Noodles",
+      quantity: 4,
+      category: "Food",
+      usedCount: 12,
+      expiryDate:
+      DateTime.now().add(
+        const Duration(days: 20),
+      ),
+    ),
+
   ];
 
+  void addItem(
+      String name,
+      int qty,
+      String category,
+      ) {
+
+    setState(() {
+
+      items.add(
+
+        ItemModel(
+
+          name: name,
+          quantity: qty,
+          category: category,
+
+          usedCount: 0,
+
+          expiryDate:
+          DateTime.now().add(
+            const Duration(
+                days: 90),
+          ),
+        ),
+      );
+
+    });
+  }
+
+  void restockItem(
+      ItemModel item) {
+
+    setState(() {
+      item.quantity += 10;
+    });
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+      BuildContext context) {
+
+    final screens = [
+
+      HomeScreen(
+        items: items,
+        onAdd: addItem,
+      ),
+
+      InventoryScreen(
+        items: items,
+        onAdd: addItem,
+      ),
+
+      StocksScreen(
+        items: items,
+        onRestock:
+        restockItem,
+      ),
+
+      ReportsScreen(
+        items: items,
+      ),
+
+    ];
 
     return Scaffold(
-      backgroundColor: Colors.grey.shade300,
+
+      backgroundColor:
+      Colors.grey.shade300,
 
       body: Center(
+
         child: Container(
+
           width: 390,
           height: 844,
 
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(25),
+          decoration:
+          BoxDecoration(
+
+            color:
+            Colors.white,
+
+            borderRadius:
+            BorderRadius.circular(
+                30),
+
             boxShadow: [
+
               BoxShadow(
+
                 blurRadius: 20,
-                color: Colors.black.withOpacity(.15),
-              ),
+
+                color: Colors.black
+                    .withOpacity(.15),
+              )
+
             ],
           ),
 
-          clipBehavior: Clip.antiAlias,
+          clipBehavior:
+          Clip.antiAlias,
 
           child: Scaffold(
 
-            body: screens[currentIndex],
+            body:
+            screens[
+            currentIndex],
 
-            bottomNavigationBar: NavigationBar(
+            bottomNavigationBar:
+            BottomNav(
 
-              height: 75,
-              selectedIndex: currentIndex,
+              currentIndex:
+              currentIndex,
 
-              onDestinationSelected: (value){
+              onTap:
+                  (value){
+
                 setState(() {
-                  currentIndex=value;
+
+                  currentIndex =
+                      value;
+
                 });
+
               },
-
-              destinations: const [
-
-                NavigationDestination(
-                  icon: Icon(Icons.home),
-                  label: "Home",
-                ),
-
-                NavigationDestination(
-                  icon: Icon(Icons.inventory),
-                  label: "Inventory",
-                ),
-
-                NavigationDestination(
-                  icon: Icon(Icons.shopping_cart),
-                  label: "Stock",
-                ),
-
-                NavigationDestination(
-                  icon: Icon(Icons.bar_chart),
-                  label: "Reports",
-                ),
-
-              ],
             ),
           ),
         ),
