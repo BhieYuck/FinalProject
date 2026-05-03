dbdf80b50f3e5f51f3675cf091a063771d776e8e
BhieYuck <dansecobrent@gmail.com>
2026-05-03 18:07:17 +0800

    Revise

---
+++ flutter_application_1/lib/data/app_data.dart
@@ -0,0 +1,26 @@
+List<Map<String, dynamic>> inventory = [
+
+  {
+    "name": "Rice",
+    "category": "Foods",
+    "quantity": 20,
+    "threshold": 5,
+  },
+
+  {
+    "name": "Soap",
+    "category": "Supplies",
+    "quantity": 2,
+    "threshold": 5,
+  },
+
+  {
+    "name": "Noodles",
+    "category": "Foods",
+    "quantity": 0,
+    "threshold": 5,
+  },
+
+];
+
+List<Map<String,dynamic>> transactions = [];
